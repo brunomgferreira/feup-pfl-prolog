2456036be20bce2887f0cae397954b58a50071fc
brunomgferreira <brunomgferreira17@gmail.com>
2024-11-28 19:26:03 +0000

    Initial commit

---
+++ src/game.pl
@@ -0,0 +1,8 @@
+% play()
+% initial_state(GameConfig, GameState)
+% display_game(GameState)
+% move(GameState, Move, NewGameState)
+% valid_moves(GameState, ListOfMoves)
+% game_over(GameState, Winner)
+% value(GameState, Player, Value)
+% choose_move(GameState, Level, Move)
